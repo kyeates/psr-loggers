88e3534fe9fb1ee3d8f7cc8bc280d4806fac0897
Kieran <kyeates@users.noreply.github.com>
2017-04-10 16:54:19 +1000

    Added EchoLogger.php
    A class that just echos any log output to console with a timestamp and the log level.

---
+++ EchoLogger.php
@@ -0,0 +1,141 @@
+<?php
+
+
+use Psr\Log\LoggerInterface;
+
+class EchoLogger implements LoggerInterface
+{
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function emergency($message, array $context = array())
+    {
+        $this->log('emergency', $message);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function alert($message, array $context = array())
+    {
+        $this->echoLog('emergency', $message);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function critical($message, array $context = array())
+    {
+        $this->echoLog('critical', $message);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function error($message, array $context = array())
+    {
+        $this->echoLog('error', $message);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function warning($message, array $context = array())
+    {
+        $this->echoLog('warning', $message);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function notice($message, array $context = array())
+    {
+        $this->echoLog('notice', $message);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function info($message, array $context = array())
+    {
+        $this->echoLog('info', $message);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function debug($message, array $context = array())
+    {
+        $this->echoLog('debug', $message);
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $this->echoLog('log', $message);
+    }
+    
+    private function echoLog($type, $message) {
+        echo date('Y-m-d H:i:s') . " {$type} - {$message}" . PHP_EOL;
+    }
+}
